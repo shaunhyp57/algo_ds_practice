171ed1c18f829d3ae088b3ca6bdc1bd5a7c6d559
Shauna Hyppolite <shaunhyp57@knights.ucf.edu>
2020-07-13 02:22:16 -0400

    update

---
+++ cracking_the_coding_interview/python_solutions/chapter_01-arrays_and_strings/03_URLify.py
@@ -0,0 +1,24 @@
+''' 
+Cracking The Coding Interview: 
+Ch 1 Arrays and Strings: 
+Q3 - URLify
+"Write a method to replace all spaces in a string with '%20: You may assume that the string has 
+sufficient space at the end to hold the additional characters, and that you are given the "true" length 
+of the string. (Note: If implementing in Java, please use a character array so that you can perform this 
+operation in place.)
+
+EXAMPLE
+Input: "Mr John Smith      ", 13
+Output: "Mr%20John%20Smith"
+
+Solutions:      Time Complexity:
+Brute Force     O(n log n)
+Dictionary      O(n)
+
+
+Note: this  implementation assumes that the input strings only include alphabet chars
+      and that spaces are ignored
+'''
+
+import unittest
+
